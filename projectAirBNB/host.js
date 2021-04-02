927535390594702b2f55020ae78e91a7c650c06e
quanghuydn94 <quanghuytrandn@gmail.com>
2021-04-02 23:26:53 +0700

    update hostpage

--- projectAirBNB/host.js
+++ projectAirBNB/host.js
@@ -1,6 +1,33 @@
-$(".Header__Button--Color").mousemove(function() {
-    var val1 = Event.pageX/2;
-    var val2 = Event.pageY/2;
-    var rgbColor = "rgb(" + val1 + ", " + val2 + ")";
-    $(".Header__Button--Color").css("background",rgbColor);
-})
+const carouselSlide = document.querySelector('.Carousel__Slide');
+const carouselImages = document.querySelectorAll('.Carousel__Slide  .Carousel__Item');
+
+//buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn'); 
+
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+console.log(size);
+
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
+
+
+//button listener
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length -4) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    // console.log(counter);
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
+
+});
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
+    
+});
