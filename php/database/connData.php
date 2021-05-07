4c34ba1ae20271c0e106ca763d0c837292864733
quanghuydn94 <quanghuytrandn@gmail.com>
2021-05-07 17:20:30 +0700

    connect, insert and show info user database

---
+++ php/database/connData.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "huytran";
+
+//create connection
+$connect = new mysqli($servername, $username, $password, $database);
+
+$connect->set_charset("utf8");
+
+//check connection
+if($connect->connect_error) {
+    die("Connection failed:". $connect->connect_error);
+}
+
+?>
